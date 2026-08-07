c700c66e42c73c5c8d8a16649563ad40d8c9e7cd
braejan <braejan@proton.me>
2026-08-07 16:12:02 -0500

    chore(perf): refine font preload metadata

--- src/root.tsx
+++ src/root.tsx
@@ -16,6 +16,7 @@ export default component$(() => {
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
+          fetchpriority="high"
         />
       </head>
       <body lang="es-CO">
